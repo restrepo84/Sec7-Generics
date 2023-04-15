4d43e2f623cf4caea436e9f64248fcda8d29c301
restrepo84 <112986175+restrepo84@users.noreply.github.com>
2023-04-14 23:23:50 -0600

    Another Generic lengthy

--- dist/app.js
+++ dist/app.js
@@ -2,5 +2,17 @@
 function merge(objA, objB) {
     return Object.assign(objA, objB);
 }
-const mergedObj = merge({ name: "Steve", hobbies: ["Sports", "Basketball"] }, { age: 39 });
+const mergedObj = merge({ name: "Steve", hobbies: ["Sports", "Basketball"] }, { age: 30 });
+console.log(mergedObj);
+function countAndDescribe(element) {
+    let descriptionText = "Got no value.";
+    if (element.length === 1) {
+        descriptionText = "Got 1 element.";
+    }
+    else if (element.length > 1) {
+        descriptionText = "Got " + element.length + " elements.";
+    }
+    return [element, descriptionText];
+}
+console.log(countAndDescribe(["Sports", "Cooking"]));
 //# sourceMappingURL=app.js.map
