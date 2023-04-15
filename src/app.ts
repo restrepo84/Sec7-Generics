210b7fde604fec52e903a8255a2b263526768190
restrepo84 <112986175+restrepo84@users.noreply.github.com>
2023-04-14 22:26:54 -0600

    what are generics

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+// const names: Array<string> = [];
+// // names[0].split("");
+
+// const promise: Promise<any> = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve("This is Done!");
+//   }, 2000);
+// });
+
+// promise.then(data => {
+//   data.slipt(' ');
+// })
